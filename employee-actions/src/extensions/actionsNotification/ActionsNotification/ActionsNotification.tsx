38a021e6a2f18be912cc2ece694d9b1cc2a5d3dd
Asun Sánchez <aslopez@plainconcepts.com>
2020-03-31 17:02:27 +0200

    Practice case: List View Command Set template

--- employee-actions/src/extensions/actionsNotification/ActionsNotification/ActionsNotification.tsx
+++ employee-actions/src/extensions/actionsNotification/ActionsNotification/ActionsNotification.tsx
@@ -3,6 +3,6 @@ import * as React from 'react';
 
 export class ActionsNotification extends React.Component<{}, {}> {
   public render() {
-    return <div>See these new actions!</div>;
+    return <div>See these new actions from React Component</div>;
   }
 }
